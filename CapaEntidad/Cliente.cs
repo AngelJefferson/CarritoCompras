0ed0dead9190ccc7d355413ea298f8832803c236
Angel Jefferson Sánchez Ventura <aj.sanchez@caid.gob.do>
2026-02-26 08:54:58 -0400

    Primer commit Avances Funcionales

---
+++ CapaEntidad/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    //    CREATE TABLE CLIENTE(
+    //IdCliente int primary key identity,
+    //Nombre varchar(100),
+    //Descripcion varchar(100),
+    //Correo varchar(100),
+    //Clave varchar(150),
+    //Reestablecer bit default 0,
+    //FechaRegistro datetime default getdate()
+    //)
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public bool Reestablecer { get; set; }
+    }
+}
