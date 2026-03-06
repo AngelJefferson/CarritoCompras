d3e9d33c9a997463c578b406f7ad0e60d9e955d1
Angel Jefferson Sánchez Ventura <aj.sanchez@caid.gob.do>
2026-03-06 15:36:07 -0400

    Actualizar proyecto con nuevos métodos y carpetas

--- CapaNegocio/CN_Usuarios.cs
+++ CapaNegocio/CN_Usuarios.cs
@@ -36,11 +36,30 @@ namespace CapaNegocio
                 Mensaje = "El Correo del usuario no puede ser vacio";
             }
             if (string.IsNullOrEmpty(Mensaje))
-            {
-                string clave = "test123";
-                obj.Clave = CN_Recursos.ConvertirSha256(clave);
+            {           
+                string clave = CN_Recursos.GenerarClave();
 
-                return objCapaDato.Registrar(obj, out Mensaje);
+                string asunto = "Creacion de Cuenta";                                
+                string mensaje_correo = "<h3>Su cuenta fue creada correctamente</h3><br/><p>Su contraseña para acceder es: !clave!</p>";
+                mensaje_correo = mensaje_correo.Replace("!clave!", clave);
+
+
+
+                bool respuesta = CN_Recursos.EnviarCorreo(obj.Correo, asunto, mensaje_correo);
+
+                if (respuesta) {
+
+                    obj.Clave = CN_Recursos.ConvertirSha256(clave);
+                    return objCapaDato.Registrar(obj, out Mensaje);
+                }
+                else {
+                    Mensaje = "No se puede enviar el correo";
+                    return 0;
+                }
+
+                
+
+                
             }
             else
             {
